6e3627bd3c2c180b1c46e8588be9aac5c71db7e0
JuanAvalo <juan.avalo@outlook.com>
2022-01-04 18:54:37 -0300

    first commit, unit 6 of the course: dynamic content

---
+++ routes/shop.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+const adminData = require('./admin');
+
+router.get('/', (req, res, next) => {
+  console.log(adminData.products);
+  res.render('shop');
+});
+
+module.exports = router;
