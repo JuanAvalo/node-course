5cb5712305a07953acca91926753a9dbc306a934
JuanAvalo <juan.avalo@outlook.com>
2022-01-09 20:47:34 -0300

    Implemented MVC pattern

--- app.js
+++ app.js
@@ -2,31 +2,25 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+
 const {engine} = require('express-handlebars');
 
 const app = express();
 
-// Handlebars
-// app.engine('hbs', engine( {extname: '.hbs', layoutsDir: 'views/layouts/', defaultLayout: 'main-layout'}));
-// app.set('view engine', 'hbs');
-
-// Pug
-//  app.set('view engine', 'pug')   Use Pug as template
-
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {docTitle: 'Page Not Found'});
-});
+app.use(errorController.get404);
 
 app.listen(3000);
